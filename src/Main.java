01a4d24bd7a12959227d5152762ae27a2e521866
Egor Petrov <epetrov-info@ya.ru>
2025-02-16 00:48:09 +0300

    MVP implementation

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        System.out.print("Игрок1, введите ваш ход (0 - орел, 1 - решка): ");
+        int playerGuess = Integer.parseInt(scanner.nextLine());
+        int targetValue = random.nextInt(2);
+        if (playerGuess == targetValue){
+            System.out.println("Выиграл:");
+            System.out.println("Игрок1");
+        } else {
+            System.out.println("Проиграл:");
+            System.out.println("Игрок1");
+        }
     }
 }
